c68ee442fb202163982843a6e124fe00d9434fb7
Taylor <tayloramccullough@gmail.com>
2016-12-15 22:00:22 -0800

    merge conflicts

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-<<<<<<< HEAD
+
 const Schema = mongoose.Schema;
 // const bcrypt = require ('bcrypt');
 const SALT_WORK_FACTOR = 10;
@@ -26,7 +26,7 @@ let userSchema = new Schema({
     			]
 			},
 		    admin: Boolean, //not likely to use this
-=======
+});
 // const Schema = mongoose.Schema;
 // const bcrypt = require ('bcrypt');
 // const SALT_WORK_FACTOR = 10;
@@ -52,7 +52,7 @@ let userSchema = new Schema({
 //     // 			]
 // 			},
 // 		    admin: Boolean, //not likely to use this
->>>>>>> c421c20dc5badeb0af8a26d35cbe53cf25fd1ac0
+
 			
 
 // 			email: {
@@ -73,69 +73,7 @@ let userSchema = new Schema({
 //     			default: Date.now
 //   			},
 			
-<<<<<<< HEAD
-			relationships: {
-				"Beach": {
-					question: Number,
-					score: Number
-				},
-				"Mansion": {
-					question: Number,
-					score: Number
-				},
-				"Swamp": {
-					question: Number,
-					score: Number
-				},
-				"Snow": {
-					question: Number,
-					score: Number
-				}
 
-			}
-
-});
-
-// // hash and authenticate below
-// // hash pw before it is saved
-// userSchema.pre('save', function(next){
-// 	let user = this;
-// // only hash pw if it has been modified or new
-// 	if(!user.isModified('password')) return next();
-// // generate a salt
-// bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt){
-// 	if (err) return next(err);
-// 	// hash new pw using new salt
-// 	bcrypt.hash(user.password, salt, function (err, hash){
-// 		if (err) return next(err);
-// 		// override the input password with the hashed one
-// 		user.password = hash;
-// 		next();
-// 	});
-// });
-
-// });
-// // password verification
-// userSchema.methods.comparePassword = function(password, callback) {
-//   bcrypt.compare(password, this.password, callback);
-//   		if (err) return cb(err);
-// 		cb(null, isMatch);
-// }
-
-module.exports = mongoose.model('User', userSchema);
-
-/*
-alternative code 
-to hash pw - make unique
-userSchema.methods.generateHash = function(password){
-	return bcrypt.hashSync(password, bcrypt.genSaltSync(9));
-}
-
-userSchema.methods.validPassword = function(password){
-	return bcrypt.compareSync(password, this.password);
-}
-*/
-=======
 // 			relationships: {
 // 				"Beach": {
 // 					question: Number,
@@ -206,9 +144,9 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
 	console.log("we're connected!!!!!!!");
-})
+});
 
-var userSchema = mongoose.Schema({ username: String, email: String, password: String });
+// var userSchema = mongoose.Schema({ username: String, email: String, password: String });
 
 module.exports = mongoose.model('User', userSchema);
->>>>>>> c421c20dc5badeb0af8a26d35cbe53cf25fd1ac0
+
